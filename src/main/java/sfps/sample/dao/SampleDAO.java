851c5eacb013037f617c261515fb25a3b839bda1
limheonjun <jumong40@naver.com>
2019-07-19 20:42:28 +0900

    _MVC 기본세팅 추가

---
+++ src/main/java/sfps/sample/dao/SampleDAO.java
@@ -0,0 +1,20 @@
+package sfps.sample.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import sfps.common.dao.AbstractDAO;
+
+
+@Repository("sampleDAO")
+public class SampleDAO extends AbstractDAO {
+
+	@SuppressWarnings("unchecked") 
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception{ 
+		return (List<Map<String, Object>>)selectList("sample.selectBoardList", map); 
+	}
+
+
+}
---
+++ src/main/java/sfps/sample/service/SampleServicempl.java
@@ -0,0 +1,26 @@
+package sfps.sample.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import sfps.sample.dao.SampleDAO;
+
+@Service("sampleService")
+public class SampleServicempl implements SampleService {
+
+	Logger log = Logger.getLogger(this.getClass()); 
+	
+	@Resource(name="sampleDAO") 
+	private SampleDAO sampleDAO; 
+	
+	@Override 
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception { 
+		return sampleDAO.selectBoardList(map); 
+	}
+
+}
